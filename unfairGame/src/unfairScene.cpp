70924a74822e99d5fb9dc68a59fdfbab47499706
Sebastiaan <vanspauwensebastiaan@gmail.com>
2019-11-28 21:11:29 +0100

    Start scene

---
+++ unfairGame/src/unfairScene.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sebastiaan on 28/11/2019.
+//
+
+#include "unfairScene.h"
---
+++ unfairGame/src/unfairScene.h
@@ -0,0 +1,15 @@
+//
+// Created by Sebastiaan on 28/11/2019.
+//
+
+#ifndef GBA_SPRITE_ENGINE_PROJECT_UNFAIRSCENE_H
+#define GBA_SPRITE_ENGINE_PROJECT_UNFAIRSCENE_H
+
+
+class unfairScene : public Scene
+{
+
+};
+
+
+#endif //GBA_SPRITE_ENGINE_PROJECT_UNFAIRSCENE_H
