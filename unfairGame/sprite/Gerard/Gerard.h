49cf176471b6d5f5f41f7328b3b30fbd7b069930
J3G0 <jeffrey.gorissen@hotmail.be>
2019-12-06 17:21:36 +0100

    Jump sprite gerard GRIT export of regular gerard

---
+++ unfairGame/sprite/Gerard/Gerard.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(gerard)
+
+//======================================================================
+//
+//	gerard, 32x32@8, 
+//	+ palette 32 entries, not compressed
+//	+ 16 tiles not compressed
+//	Total size: 64 + 1024 = 1088
+//
+//	Time-stamp: 2019-12-06, 17:09:32
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_GERARD_H
+#define GRIT_GERARD_H
+
+#define gerardTilesLen 1024
+extern const unsigned int gerardTiles[256];
+
+#define gerardPalLen 64
+extern const unsigned short gerardPal[32];
+
+#endif // GRIT_GERARD_H
+
+//}}BLOCK(gerard)
