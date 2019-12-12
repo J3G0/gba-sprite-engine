568d097f5036d87c582b7687216d5622f0499215
J3G0 <jeffrey.gorissen@hotmail.be>
2019-12-12 12:19:46 +0100

    Gerard update

---
+++ unfairGame/sprite/Fireball.c
@@ -0,0 +1,22 @@
+
+//{{BLOCK(Fireball)
+
+//======================================================================
+//
+//	Fireball, 8x8@8, 
+//	+ 1 tiles not compressed
+//	Total size: 64 = 64
+//
+//	Time-stamp: 2019-12-12, 12:05:09
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+const unsigned int FireballTiles[16] __attribute__((aligned(4)))=
+{
+	0x090A0808,0x00000A0A,0x090A0008,0x0008080A,0x090A0A08,0x0008000A,0x090A0800,0x00080A09,
+	0x090A0800,0x0000080A,0x090A0800,0x00080A09,0x0A0A0A08,0x00080A0A,0x08080800,0x00000808,
+};
+
+//}}BLOCK(Fireball)
---
+++ unfairGame/sprite/Fireball.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(Fireball)
+
+//======================================================================
+//
+//	Fireball, 8x8@8, 
+//	+ 1 tiles not compressed
+//	Total size: 64 = 64
+//
+//	Time-stamp: 2019-12-12, 12:05:09
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_FIREBALL_H
+#define GRIT_FIREBALL_H
+
+#define FireballTilesLen 64
+extern const unsigned int FireballTiles[16];
+
+#endif // GRIT_FIREBALL_H
+
+//}}BLOCK(Fireball)
